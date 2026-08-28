c1905d0f229739c7bb624b4e1a4153ed06a545dc
Giacomo Queirolo <giacomo.queirolo@umontpellier.fr>
2026-08-28 12:17:48 +0200

    stat_gamma_1Ddens.py: debugged and modularised

--- src/nazgul/stat_gamma_1Ddens.py
+++ src/nazgul/stat_gamma_1Ddens.py
@@ -1,15 +1,16 @@
 # Study statistically core and power law index from the 1D mass distribution
 # to add: compare with lens model results
 import sys
+import dill
 import warnings
 import argparse
-import numpy as np
 import matplotlib
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from importlib import import_module
 from scipy.optimize import curve_fit
-from chainconsumer import Chain #, ChainConsumer
-from chainconsumer.plotting import plot_contour,plot_dist #,plot_truths
+from chainconsumer import Chain, ChainConsumer
 
 from python_tools.get_res import load_whatever
 from nazgul.mount_doom.lens_system import LensSystem
@@ -42,138 +43,149 @@ def log_rho_cored(log_r,log_rho0,log_r_core,gamma):
 matplotlib.use('Agg') 
 warm         = ['#fdcc8a', '#fc8d59', '#d7301f']
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(prog=sys.argv[0],description=" Study statistically core and power law index from the 1D mass distribution")
-    parser.add_argument('-nl','--n_lenses',type=int,dest="n_lenses",default=5,help=f"Number of lenses to model")
-    parser.add_argument('-mtE','--min_thetaE',type=float,dest="min_thetaE",default=None,help=f"Min theta_E for the gal to be considered a lens")
-    parser.add_argument('-snap','--snap',nargs="+",type=str,dest="snaps",default=[],help=f"List of snaps to consider - default is all")
-    parser.add_argument('-sim','--sim',type=str,dest="sim",default=std_sim,help=f"Simulation name")
-    parser.add_argument('-ss','--simsuite',type=str,dest="simsuite",default=std_simsuite,help=f"Simulation suite name")
-    parser.add_argument('-ssim','--subsim',type=str,dest="subsim",default=std_subsim,help=f"Sub-Simulation name")
-    parser.add_argument('-lm','--lens_model',type=str,dest="lens_model",default=std_lens_model,help=f"Lens model to compare the power law to (and get the lens list from)")
-    parser.add_argument('-nsp','--no_single_plot',dest="single_plot",
-                        default=True,action="store_false",help=f"Do not plot each 1D density fit")
-
-    args        = parser.parse_args()
-    n_lenses    = args.n_lenses
-    min_thetaE  = args.min_thetaE 
-    snaps       = args.snaps #[25,26,27]
-    sim         = args.sim
-    subsim      = args.subsim
-    simsuite    = args.simsuite
-    lens_model  = args.lens_model
-    single_plot = args.single_plot
-    
-    # picked by hand "bad" lenses ->
-    lenses2skip = ["LS_Lens_Gn75SGn0_Prj1","LS_Lens_Gn4SGn0_Prj2","LS_Lens_Gn4SGn0_Prj0","LS_Lens_Gn14SGn0_Prj1",
-                   "LS_Lens_Gn71SGn0_Prj2","LS_Lens_Gn7SGn1_Prj2","LS_Lens_Gn15SGn1_Prj0","LS_Lens_Gn15SGn1_Prj0",
-                   "LS_Lens_Gn6SGn0_Prj2","LS_Lens_Gn1SGn2_Prj1","LS_Lens_Gn42SGn0_Prj1","LS_Lens_Gn18SGn0_Prj2",
-                   "LS_Lens_Gn18SGn0_Prj0","LS_Lens_Gn18SGn0_Prj0","LS_Lens_Gn18SGn0_Prj2","LS_Lens_Gn22SGn1_Prj2",
-                   "LS_Lens_Gn22SGn1_Prj1","LS_Lens_Gn66SGn0_Prj1","LS_Lens_Gn45SGn0_Prj0","LS_Lens_Gn33SGn0_Prj2"]
-
+def get_res_dir(lens_model):
     warnings.warn(f"Using lens model: {lens_model}")
     module_path = f"nazgul.Modelling.model_{lens_model}"
     model_module = import_module(module_path)
     res_dir = getattr(model_module,"res_dir_base")
-    
-    kw_get_all_gallens = {"sim":sim,
-                          "subsim":subsim,
-                          "simsuite":simsuite,
-                          "snaps":snaps}
+    return res_dir    
+
+std_kw_get_all_gallens = {"sim":std_sim,
+                          "subsim":std_subsim,
+                          "simsuite":std_simsuite,
+                          "snaps":[]}
+
+r_lbl = r"log$_{10}$r [kpc]"
+sig_lbl = r"log$_{10}\Sigma$ [10$^9$ M$_{\odot}$/kpc]"
+
+def get_core_gamma_1gal(gal_lens,single_plot=True):
+    print("\nLoading gal lens "+gal_lens.name)
+    gal_lens.unpack()
+    kw_1d = get_kw_1D_density(gal_lens.Gal,gal_lens.proj_index)
+            
+    r = kw_1d["r_all"].value #kpc
+    Sigma = kw_1d["Sigma_encl_all"].value
+    Sigma_scaled = Sigma/1e9 # 1e9 SolMass/kpc
+            
+    fit_prm,fit_cov = curve_fit(rho_cored,r,Sigma_scaled)
+    rho0    = fit_prm[0]
+    core    = fit_prm[1]
+    gamma   = fit_prm[2]
+    print("Results from 1D density Cored Power Law fit")
+    print("Core :",np.round(core,2))
+    print("Gamma :",np.round(gamma,2))
+    log_r,log_Sig = np.log10(r),np.log10(Sigma_scaled)
+    savedir = get_savedir_plots(gal_lens.Gal)
+    if single_plot:
+        nm_fig = f"{savedir}/1D_Density_fit.png"
+        # if plot doesn't exits (to implement)  
+        fig,ax = plt.subplots()
+        ax.set_title("Density profile fit with cored power law")
+        ax.plot(log_r,log_Sig,c="b",label="data")
+        ax.plot(log_r,np.log10(rho_cored(r,*fit_prm)),c="g",
+                 ls="--",label="fit cored PL")
+        ax.set_xlabel(r_lbl)
+        ax.set_ylabel(sig_lbl)
+        ax.axvline(np.log10(core),ls="-.",c="g",label="core")
+        ax.legend()
+        fig.savefig(nm_fig)
+        print(f"Saving {nm_fig}")
+        plt.close(fig)
+
+    # get gamma from lens modelling to compare it
+    # the following is just used for convenience
+    lens = LensSystem.from_GalLens(gal_lens)
+    gal_lens.model_res_dir = get_model_res_dir(lens,res_dir=res_dir)
+    try:
+        full_chain = get_full_chain(gal_lens,model=lens_model)
+        gamma_lns = full_chain["gamma_lens0"].to_numpy() 
+        gamma_med,gamma_std = np.median(gamma_lns),np.std(gamma_lns)
+        gamma_lens = [gamma_med,gamma_std]
+        del full_chain,gamma_lns
+    except FileNotFoundError as e:
+        warnings.warn(f"{e} \nWe ignore it and set it to NaN")
+        gamma_lens = [np.nan,np.nan]
+    del gal_lens,lens
+    # for plot overlap of 1D density profiles (normalised by core and max dens) 
+    log_r_scaled   = log_r/np.log10(core)
+    log_Sig_scaled = log_Sig/np.log10(rho0)
+    return gamma,core,gamma_lens,log_r_scaled,log_Sig_scaled
+
+def get_core_gamma(reload=True,
+                    res_dir=None,  
+                    min_thetaE=None,
+                    single_plot=True,
+                    lenses2skip=[],
+                    kw_get_all_gallens=std_kw_get_all_gallens):
+    nm_kw_core_gamma = f"{res_dir}/kw_res_core_gamma.dll"
+    if reload:
+        try:
+            kw_core_gamma = load_whatever(nm_kw_core_gamma)
+            print(f"Loaded {nm_kw_core_gamma}") 
+            return kw_core_gamma
+        except FileNotFoundError:
+            print("Tried and failed to load previous result. Recomputing")
 
     gal_lenses  = get_lenses2model(res_dir=res_dir,
                                    reload=True,
-                                   n_lenses=np.nan, # has to load all of them anyway
                                    min_thetaE=min_thetaE,
                                    skip_lenses=lenses2skip,
                                    kw_get_all_gallens=kw_get_all_gallens)
-    log_r_scaled,log_Sig_scaled = [],[]
     gammas,cores = [],[]
-    gamma_lens   = []
+    gamma_lenses   = []
     fig_overlap,ax_overlap = plt.subplots()
     nm_fig_overap = f"{res_dir}/1D_overlap.png"
-    r_lbl = r"log$_{10}$r [kpc]"
-    sig_lbl = r"log$_{10}\Sigma$ [10$^9$ M$_{\odot}$/kpc]"
     ax_overlap.set_xlabel(r_lbl)
     ax_overlap.set_ylabel(sig_lbl)
     ax_overlap.set_title("Overlap of scaled 1D density profiles")
     
     for i,gal_lens in enumerate(gal_lenses): 
-        print("\nLoading gal lens "+gal_lens.name)
         
-        gal_lens.unpack()
-        kw_1d = get_kw_1D_density(gal_lens.Gal,gal_lens.proj_index)
-                
-        r = kw_1d["r_all"].value #kpc
-        Sigma = kw_1d["Sigma_encl_all"].value
-        Sigma_scaled = Sigma/1e9 # 1e9 SolMass/kpc
-                
-        fit_prm,fit_cov = curve_fit(rho_cored,r,Sigma_scaled)
-        rho0    = fit_prm[0]
-        core    = fit_prm[1]
-        gamma   = fit_prm[2]
-        print("Results from 1D density Cored Power Law fit")
-        print("Core :",np.round(core,2))
-        print("Gamma :",np.round(gamma,2))
-        log_r,log_Sig = np.log10(r),np.log10(Sigma_scaled)
-        savedir = get_savedir_plots(gal_lens.Gal)
-        if single_plot:
-            nm_fig = f"{savedir}/1D_Density_fit.png"
-            # if plot doesn't exits (to implement)        
-            plt.plot(log_r,log_Sig,c="b",label="data")
-            plt.plot(log_r,np.log10(rho_cored(r,*fit_prm)),c="g",
-                     ls="--",label="fit cored PL")
-            plt.xlabel(r_lbl)
-            plt.ylabel(sig_lbl)
-            plt.axvline(np.log10(core),ls="-.",c="g",label="core")
-            plt.legend()
-            plt.savefig(nm_fig)
-            print(f"Saving {nm_fig}")
-            plt.close()
-        # plot overlap of 1D density profiles (normalised by core and max dens) 
-        _log_r_scaled   = log_r/np.log10(core)
-        _log_Sig_scaled = log_Sig/np.log10(rho0)
+        gamma,core,gamma_lens,log_r_scaled,log_Sig_scaled = get_core_gamma_1gal(gal_lens,
+                                                                  single_plot=single_plot)
+
         _lbl = None
         if i==0:
             _lbl = "N = "+str(len(gal_lenses))
-        ax_overlap.plot(_log_r_scaled,_log_Sig_scaled,
+        ax_overlap.plot(log_r_scaled,log_Sig_scaled,
                         alpha=.5,c="grey",label=_lbl)
 
-        # get gamma from lens modelling and compare
-
-        # the following is just used for convenience
-        lens = LensSystem.from_GalLens(gal_lens)
-        gal_lens.model_res_dir = get_model_res_dir(lens,res_dir=res_dir)
-        try:
-            full_chain = get_full_chain(gal_lens,model=lens_model)
-            gamma_lns = full_chain["gamma_lens0"].to_numpy() 
-            gamma_med,gamma_std = np.median(gamma_lns),np.std(gamma_lns)
-            gamma_lens.append([gamma_med,gamma_std])
-            del full_chain,gamma_lns
-        except FileNotFoundError as e:
-            
-            warnings.warn(f"{e} \nWe ignore it and set it to NaN")
-            gamma_lens.append([np.nan,np.nan])
         # store res
         cores.append(core)
         gammas.append(gamma)
-        log_r_scaled.append(_log_r_scaled)
-        log_Sig_scaled.append(_log_Sig_scaled)
-        
-        del gal_lens,lens
+        gamma_lenses.append(gamma_lens)
         
     fig_overlap.savefig(nm_fig_overap)
     print(f"Saving {nm_fig_overap}")
     plt.close(fig_overlap)
+
+    gammas = np.array(gammas)
+    cores  = np.array(cores)
+    gamma_lenses = np.array(gamma_lenses).T # [0] =<gamma>, [1] = std(gamma)
+    kw_core_gamma = {"gammas":gammas,
+                     "cores":cores,
+                     "gamma_lenses":gamma_lenses}
+
+    with open(nm_kw_core_gamma,"wb") as f:
+        dill.dump(kw_core_gamma,f)
+    print(f"Saved {nm_kw_core_gamma}") 
+    return kw_core_gamma
+
+def plot_core_gamma(kw_core_gamma,res_dir):
+    cores = kw_core_gamma["cores"]
+    gammas = kw_core_gamma["gammas"]
+    gamma_lenses = kw_core_gamma["gamma_lenses"]
     
     # plot summ. stat of core and gamma
-    chain4plotcont = Chain(samples=np.array([cores,gammas]), 
-                           name="", 
-                           shade=True, 
-                           color=warm[1], 
-                           shade_gradient = 0.8, linewidth=3.0)
-    fig,ax = plt.subplots()
-    plot_contour(ax, chain4plotcont, px="core", py=r"$\gamma$")
+    c = ChainConsumer()
+    prms_nms = ["Core",r"$\gamma$"]
+    c.add_chain(Chain(
+        samples=pd.DataFrame(np.array([cores,gammas]).T,columns=prms_nms),
+        name="",
+        shade=True,
+        color=warm[0],
+        shade_gradient = 0.8, linewidth=3.0) )
+    fig = c.plotter.plot(columns=prms_nms)
     nm_fig = f"{res_dir}/distr_coreVsGamma_1Ddens.png"
     plt.savefig(nm_fig)
     print(f"Saving {nm_fig}")
@@ -186,17 +198,61 @@ if __name__=="__main__":
     plt.ylabel(r"log$_{10}$r$_{\rm{core}}$ [kpc]")
     plt.xlabel(r"$\gamma$")
     nm_fig = f"{res_dir}/dens_1dfit_gammaVs_core.png"
-    plt.savefig()
+    plt.savefig(nm_fig)
     print(f"Saving {nm_fig}")
+    plt.close()
     
     # compare gammas
-    plt.errorbar(gammas[ind_sort],gamma_lens[0][ind_sort],
-                 yerr=gamma_lens[1][ind_sort])
-    plt.scatter(gammas[ind_sort],gamma_lens[0][ind_sort],c="k")
+    plt.errorbar(gammas[ind_sort],gamma_lenses[0][ind_sort],
+                 yerr=gamma_lenses[1][ind_sort])
+    plt.scatter(gammas[ind_sort],gamma_lenses[0][ind_sort],c="k")
     plt.title(r"$\gamma_{\rm{Density}}$ vs $\gamma_{\rm{PEMD}}$")
     plt.xlabel(r"$\gamma_{\rm{Density}}$")
     plt.ylabel(r"$\gamma_{\rm{Lens Model}}$")
     nm_fig = f"{res_dir}/gamma_dens_Vs_gamma_lens.png"
-    plt.savefig()
+    plt.savefig(nm_fig)
     print(f"Saving {nm_fig}")
+    plt.close()
     
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(prog=sys.argv[0],description=" Study statistically core and power law index from the 1D mass distribution")
+    parser.add_argument('-mtE','--min_thetaE',type=float,dest="min_thetaE",default=None,help=f"Min theta_E for the gal to be considered a lens")
+    parser.add_argument('-snap','--snap',nargs="+",type=str,dest="snaps",default=[],help=f"List of snaps to consider - default is all")
+    parser.add_argument('-sim','--sim',type=str,dest="sim",default=std_sim,help=f"Simulation name")
+    parser.add_argument('-ss','--simsuite',type=str,dest="simsuite",default=std_simsuite,help=f"Simulation suite name")
+    parser.add_argument('-ssim','--subsim',type=str,dest="subsim",default=std_subsim,help=f"Sub-Simulation name")
+    parser.add_argument('-lm','--lens_model',type=str,dest="lens_model",default=std_lens_model,help=f"Lens model to compare the power law to (and get the lens list from)")
+    parser.add_argument('-nsp','--no_single_plot',dest="single_plot",
+                        default=True,action="store_false",help=f"Do not plot each 1D density fit")
+    parser.add_argument('-nr','--no_reload',dest="reload",
+                        default=True,action="store_false",help=f"Do not try to reload prev. res.")
+
+    args        = parser.parse_args()
+    min_thetaE  = args.min_thetaE 
+    snaps       = args.snaps #[25,26,27]
+    sim         = args.sim
+    subsim      = args.subsim
+    simsuite    = args.simsuite
+    lens_model  = args.lens_model
+    single_plot = args.single_plot
+    reload      = args.reload
+    
+    # picked by hand "bad" lenses ->
+    lenses2skip = ["LS_Lens_Gn75SGn0_Prj1","LS_Lens_Gn4SGn0_Prj2","LS_Lens_Gn4SGn0_Prj0","LS_Lens_Gn14SGn0_Prj1",
+                   "LS_Lens_Gn71SGn0_Prj2","LS_Lens_Gn7SGn1_Prj2","LS_Lens_Gn15SGn1_Prj0","LS_Lens_Gn15SGn1_Prj0",
+                   "LS_Lens_Gn6SGn0_Prj2","LS_Lens_Gn1SGn2_Prj1","LS_Lens_Gn42SGn0_Prj1","LS_Lens_Gn18SGn0_Prj2",
+                   "LS_Lens_Gn18SGn0_Prj0","LS_Lens_Gn18SGn0_Prj0","LS_Lens_Gn18SGn0_Prj2","LS_Lens_Gn22SGn1_Prj2",
+                   "LS_Lens_Gn22SGn1_Prj1","LS_Lens_Gn66SGn0_Prj1","LS_Lens_Gn45SGn0_Prj0","LS_Lens_Gn33SGn0_Prj2"]
+
+    res_dir            = get_res_dir(lens_model)
+    kw_get_all_gallens = {"sim":sim,
+                          "subsim":subsim,
+                          "simsuite":simsuite,
+                          "snaps":snaps}
+    kw_core_gamma = get_core_gamma(reload=reload,
+                                   res_dir = res_dir,  
+                                   min_thetaE=min_thetaE,
+                                   kw_get_all_gallens = kw_get_all_gallens,
+                                   single_plot=single_plot,
+                                   lenses2skip=lenses2skip)
+    plot_core_gamma(kw_core_gamma,res_dir)
